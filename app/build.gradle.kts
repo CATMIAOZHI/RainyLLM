a81d23afef87bdba11995752437bc45a6184bd92
CATMIAOZHI <CATMIAOZHI@github.com>
2026-04-29 13:26:48 +0000

    fix: 改用Debug构建避免签名问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,6 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.debug
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
